a7def9e30dfa69e3b8c77a3d136aa4df4c3943ec
PabasaraGamage <148269929+PabasaraGamage@users.noreply.github.com>
2024-08-16 14:07:23 +0530

    changed end point name

--- src/main/java/com/example/simple_site/WebController.java
+++ src/main/java/com/example/simple_site/WebController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WebController {
 
-    @GetMapping("/")
+    @GetMapping("/hello")
     public String home(Model model) {
         model.addAttribute("message", "Welcome to Simple Site!");
         return "index";
