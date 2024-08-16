6cb6e0163d38eb77e7eabaa1b3e23999e366aaf7
PabasaraGamage <148269929+PabasaraGamage@users.noreply.github.com>
2024-08-16 11:09:33 +0530

    First commit

---
+++ src/main/java/com/example/simple_site/SimpleSiteApplication.java
@@ -0,0 +1,12 @@
+package com.example.simple_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSiteApplication.class, args);
+    }
+}
